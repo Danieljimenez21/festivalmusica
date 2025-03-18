de05a636ec481a0f4d9166b4ff0ea7909b8c1fd3
Daniel_Jimenez <djimeneza7b2276@gmail.com>
2025-03-18 16:53:01 -0600

    compilando SASS con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-export function hola(done) {
-    console.log("Hola desde GULP")
-    
+import {src, dest} from 'gulp'
+
+import * as dartSass from 'sass'
+
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ){
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css'))
+
     done()
 }
